bf35d89418a6003df3f3bc552b034b833038dd8b
Umid <shresthaumidccc@gmail.com>
2025-10-26 11:26:39 +0545

    updated urls.py

--- URLShortener/urls.py
+++ URLShortener/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +10,9 @@ urlpatterns = [
     path('', include('shortener.urls')),
 ]
 
-#test for deplyment
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
